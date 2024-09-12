d13859e775a4ac05fa992bd4a4d9ada50cce8ce9
mamomen <47065311+mamomen@users.noreply.github.com>
2024-09-12 13:44:14 +0600

    Update Basic_Networking_Interview_Questions.py

--- Basic_Networking_Interview_Questions.py
+++ Basic_Networking_Interview_Questions.py
@@ -1,7 +1,4 @@
-
-
-
-#Basic_Networking_Interview_Questions
+# Basic_Networking_Interview_Questions
 
 do_you_play = input('Do You Play _______Y/N___? ').lower()
 
@@ -12,33 +9,32 @@ print('Lets Play')
 count = 0
 
 Question_1 = input('What is the FTP protocol ? ').upper()
-if Question_1 =='FTP':
+if Question_1 == 'FTP':
     print('Wow, Correct Answer')
     count += 1
 else:
     print('Sorry,Incorrect Answer')
 
 Question_2 = input('What is the MAC ? ').title()
-if Question_2 =='Media Access Control':
+if Question_2 == 'Media Access Control':
     print('Wow, Correct Answer')
     count += 1
 else:
     print('Sorry,Incorrect Answer')
 
 Question_3 = input('What is the ARP ? ').title()
-if Question_3 =='Address Resolution Protocol':
+if Question_3 == 'Address Resolution Protocol':
     print('Wow, Correct Answer')
     count += 1
 else:
     print('Sorry,Incorrect Answer')
 
 Question_3 = input('What is the TCP protocol ? ').title()
-if Question_3 =='Transmission Control Protocol':
+if Question_3 == 'Transmission Control Protocol':
     print('Wow, Correct Answer')
     count += 1
 else:
     print('Sorry,Incorrect Answer')
 
-
-print("You Got" + " " +  str(count) + " " + "Out of 4")
-print("which is" + " " + str((count/4 *100 ))+ "%")
+print("You Got" + " " + str(count) + " " + "Out of 4")
+print("which is" + " " + str((count / 4 * 100)) + "%")
